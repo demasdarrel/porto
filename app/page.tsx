c60ecada8bacb00349e2a8def9752757afa0692a
demasdarrel <demasdarrel.krw@gmail.com>
2025-08-13 08:30:01 +0700

    fix: add custom easeOutCubic function for SplitText animation

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,6 @@ export default function Home() {
                 delay={10}
                 animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
                 animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-                easing="easeOutCubic"
                 threshold={0.2}
                 rootMargin="-50px"
                 />
